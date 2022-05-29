359e689d7215f3b66d89d44d02af6468e78e6ddd
sainath <sainath.mannem@gmail.com>
2022-05-29 10:22:06 -0700

    Updated for Collection List

---
+++ src/com/sainath/java/examples/collection/CollectionExam.java
@@ -0,0 +1,27 @@
+package com.sainath.java.examples.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionExam {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		list.add(2);
+		list.add(3);
+		int i = list.get(0);
+		for (Integer integer : list) {
+			System.out.println(integer);
+			list.remove(integer);
+		}
+		Iterator<Integer> iterator = list.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+			iterator.remove();
+		}
+		
+		
+		
+	}
+}
